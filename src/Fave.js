856adfdb8fd15ff6c02793d67cfc2ace3e5f84e4
Abby Christens <achristens@gmail.com>
2017-12-16 11:43:34 -0500

    Adding conditional classes to Fave

--- src/Fave.js
+++ src/Fave.js
@@ -2,14 +2,26 @@ import React, { Component } from 'react';
 
 class Fave extends Component{
 
+  constructor(props){
+    super()
+    this.state = {
+      isFave: false,
+    }
+    this.handleClick = this.handleClick.bind(this);
+  }
   handleClick(e) {
-    e.preventDefault()
+    e.stopPropagation()
     console.log("handling Fave click!")
+    this.setState({
+      isFave: !this.state.isFave
+    })
+
   }
 
   render(){
+
     return(
-      <div onClick={this.handleClick} className="film-row-fave add_to_queue">
+      <div onClick={this.handleClick} className={"film-row-fave " + (this.state.isFave ? 'remove_from_queue' : 'add_to_queue')}>
         <i className="material-icons">add_to_queue</i>
       </div>
     )
