85286acd4748d98befb237b6026f42a38319405c
Abby Christens <achristens@gmail.com>
2017-12-16 12:24:17 -0500

    Toggling is-active class on filter divs

--- src/FilmListing.js
+++ src/FilmListing.js
@@ -3,8 +3,29 @@ import FilmRow from './FilmRow';
 
 class FilmListing extends Component{
 
-  handleFaveClick(filter){
-    console.log(`Setting filter to ${filter}`)
+  constructor(props){
+    super()
+    this.state = {
+      filter: 'all'
+    }
+    this.handleFilterClick = this.handleFilterClick.bind(this);
+  }
+
+  handleFilterClick(e, filter){
+    var faves = document.getElementById('faves-div')
+    var all   = document.getElementById('all-div')
+
+    if (e.target.innerText.startsWith('ALL') === true){
+      e.target.className = "film-list-filter is-active";
+      faves.className = "film-list-filter"
+    } else if (e.target.innerText.startsWith("FAVES") === true){
+      e.target.className = "film-list-filter is-active";
+      all.className = "film-list-filter"
+    }
+    this.setState({
+      filter: filter
+    })
+
   }
 
   render(){
@@ -12,7 +33,6 @@ class FilmListing extends Component{
     let filmRows = films.map((film, key)=> (
       <FilmRow key={film.id} film={film}/>
     ))
-
     return(
       <div className="film-list">
         <h1 className="section-title">
@@ -20,11 +40,11 @@ class FilmListing extends Component{
         </h1>
 
         <div className="film-list-filters">
-          <div className="film-list-filter" onClick={(e) => this.handleFaveClick('all')}>
+          <div id="all-div" className="film-list-filter" onClick={(e) => this.handleFilterClick(e, 'all')}>
             ALL
             <span className="section-count">{films.length}</span>
           </div>
-          <div className="film-list-filter" onClick={(e) => this.handleFaveClick('faves')}>
+          <div id="faves-div" className="film-list-filter" onClick={(e) => this.handleFilterClick(e, 'faves')}>
             FAVES
             <span className="section-count">0</span>
           </div>
